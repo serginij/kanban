d63390d4d2cde398adfcab3febedcde0bc9322ed
serginij <kotlizkiy@gmail.com>
2019-05-07 23:17:07 +0300

    Add redux & react-redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { configureStore } from './store'
 import '@babel/polyfill'
 
+import { Provider } from 'react-redux'
 import { App } from '@components/app'
+
+const store = configureStore()
 
 const root = document.getElementById('root')
 
-ReactDOM.render(<App />, root)
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  root
+)

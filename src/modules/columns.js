5d88f3aa6a68522f8de490df37c52f3458e911ec
serginij <kotlizkiy@gmail.com>
2019-05-12 00:00:01 +0300

    Fix column module

--- src/modules/columns.js
+++ src/modules/columns.js
@@ -1,67 +1,70 @@
-const nanoid = require('nanoid')
+// const nanoid = require('nanoid')
+import nanoid from 'nanoid'
 
 const ADD_COLUMN = '@@columns/add'
 const DELETE_COLUMN = '@@columns/delete'
 const RENAME_COLUMN = '@@columns/rename'
 
-export const addColumn = name => {
-  return {
-    type: ADD_COLUMN,
-    name,
-    id: nanoid(8)
-  }
-}
+export const addColumn = name => ({
+  type: ADD_COLUMN,
+  name,
+  id: nanoid(8)
+})
 
-export const deleteColumn = id => {
-  return {
-    type: DELETE_COLUMN,
-    columnId: id
-  }
-}
+export const deleteColumn = id => ({
+  type: DELETE_COLUMN,
+  id
+})
 
-export const renameColumn = (name, id) => {
-  return {
-    type: RENAME_COLUMN,
-    name,
-    id: id
-  }
-}
+export const renameColumn = (name, id) => ({
+  type: RENAME_COLUMN,
+  name,
+  id
+})
 
 const initialState = {
-  columnsById: {},
-  allColumns: []
+  columns: {
+    columnsById: {},
+    allColumns: []
+  }
 }
 
 export const columns = (state = initialState, action) => {
   switch (action.type) {
     case ADD_COLUMN:
-      const columns = state.columnsById
-      columns[action.id] = {
+      const newColumn = {
         name: action.name,
         cards: []
       }
+
       return {
-        ...state,
-        columnsById: columns
-      }
-    case RENAME_COLUMN:
-      const column = state.columnsById[action.id]
-      column.name = action.name
-      return {
-        ...state,
-        columnsById: {
-          ...this,
-          [action.id]: column
+        columns: {
+          ...state.columns,
+          columnsById: [...state.columns.columnsById, newColumn]
         }
       }
+
+    case RENAME_COLUMN:
+      return {
+        columns: {
+          ...state.columns,
+          columnsById: {
+            ...state.columns.columnsById,
+            [action.id]: {
+              ...state.columns.columnsById[action.id],
+              name: action.name
+            }
+          }
+        }
+      }
+
     case DELETE_COLUMN:
       let { columnsById, allColumns } = state
       let { [action.id]: deletedColumn, ...rest } = columnsById
-      allColumns.filter(columnId => columnId !== action.id)
       return {
         ...state,
         columnsById: rest,
-        allColumns: allColumns
+        allColumns: allColumns.filter(columnId => columnId !== action.id)
       }
     default:
       return state

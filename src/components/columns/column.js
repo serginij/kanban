dc8df4c43c709955a0b350d5b22285e03a5f81b9
serginij <kotlizkiy@gmail.com>
2019-05-13 18:41:13 +0300

    Add redux to components

--- src/components/columns/column.js
+++ src/components/columns/column.js
@@ -1,22 +1,40 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { addCard } from '@modules/cards'
 
 import { Card } from '@components/card'
 
-const testCards = ['One', 'Two', 'Three']
-
 export const Column = props => {
-  const cards = testCards.map((card, index) => (
-    <li key={index}>
-      <Card text={card} />
-    </li>
-  ))
+  const [text, setText] = React.useState('')
+  const cards = useSelector(state => state.cards.cardsById)
+  const dispatch = useDispatch()
+
+  const handleAddCard = id => {
+    dispatch(addCard(text, id))
+    setText('')
+  }
+  const cardsList = Object.values(props.cards).map(id => {
+    let card = cards[id]
+    return (
+      <li key={card._id}>
+        <Card text={card.name} />
+      </li>
+    )
+  })
   return (
     <article>
       <b>{props.name}</b>
-      <ul>{cards}</ul>
+      <ul>{cardsList}</ul>
       <article>
-        <input type="text" placeholder="Введите название карточки" />
-        <button>Добавить карточку</button>
+        <input
+          value={text}
+          type="text"
+          placeholder="Введите название карточки"
+          onChange={e => setText(e.target.value)}
+        />
+        <button onClick={() => handleAddCard(props.columnId)}>
+          Добавить карточку
+        </button>
       </article>
     </article>
   )
--- src/components/columns/index.js
+++ src/components/columns/index.js
@@ -1,19 +1,38 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { addColumn } from '@modules/columns'
 
 import { Column } from './column'
 
-const testColumns = ['First', 'Second']
-
 export const Columns = () => {
-  const columns = testColumns.map((column, index) => (
-    <Column name={column} key={index} />
+  const [name, setName] = React.useState('')
+  const columns = useSelector(state => state.columns.columnsById)
+  const dispatch = useDispatch()
+
+  const handleAddColumn = () => {
+    dispatch(addColumn(name))
+    setName('')
+  }
+
+  const columnsList = Object.values(columns).map(column => (
+    <Column
+      name={column.name}
+      cards={column.cards}
+      key={column._id}
+      columnId={column._id}
+    />
   ))
   return (
     <section>
-      {columns}
+      {columnsList}
       <article>
-        <input type="text" placeholder="Введите название колонки" />
-        <button>Добавить колонку</button>
+        <input
+          value={name}
+          type="text"
+          placeholder="Введите название колонки"
+          onChange={e => setName(e.target.value)}
+        />
+        <button onClick={() => handleAddColumn()}>Добавить колонку</button>
       </article>
     </section>
   )

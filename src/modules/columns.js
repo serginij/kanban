89e9f83eb400f14db9631b60b8ad42fdd0cc25a6
serginij <kotlizkiy@gmail.com>
2019-05-18 13:57:25 +0300

    Remove edit & delete actions

--- src/modules/columns.js
+++ src/modules/columns.js
@@ -1,26 +1,13 @@
 import nanoid from 'nanoid'
 
-import { ADD_CARD, DELETE_CARD } from './cards'
+import { ADD_CARD } from './cards'
 
 const ADD_COLUMN = '@@columns/add'
-const DELETE_COLUMN = '@@columns/delete'
-const RENAME_COLUMN = '@@columns/rename'
 
 export const addColumn = name => ({
   type: ADD_COLUMN,
   name,
   id: nanoid(8)
-})
-
-export const deleteColumn = id => ({
-  type: DELETE_COLUMN,
-  id
-})
-
-export const renameColumn = (name, id) => ({
-  type: RENAME_COLUMN,
-  name,
-  id
 })
 
 const initialState = {
@@ -43,27 +30,6 @@ export const columns = (state = initialState, action) => {
         allColumns: [...state.allColumns, action.id]
       }
 
-    case RENAME_COLUMN:
-      return {
-        ...state,
-        columnsById: {
-          ...state.columnsById,
-          [action.id]: {
-            ...state.columnsById[action.id],
-            name: action.name
-          }
-        }
-      }
-
-    case DELETE_COLUMN:
-      const { columnsById, allColumns } = state
-      const { [action.id]: deletedColumn, ...rest } = columnsById
-      return {
-        ...state,
-        columnsById: rest,
-        allColumns: allColumns.filter(columnId => columnId !== action.id)
-      }
-
     case ADD_CARD:
       return {
         ...state,
@@ -72,20 +38,6 @@ export const columns = (state = initialState, action) => {
           [action.columnId]: {
             ...state.columnsById[action.columnId],
             cards: [...state.columnsById[action.columnId].cards, action.id]
-          }
-        }
-      }
-
-    case DELETE_CARD:
-      return {
-        ...state,
-        columnsById: {
-          ...state.columnsById,
-          [action.columnId]: {
-            ...state.columnsById[action.columnId],
-            cards: state.columnsById[action.columnId].cards.filter(
-              cardId => cardId !== action.id
-            )
           }
         }
       }
